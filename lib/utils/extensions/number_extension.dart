ffec80ca3a3bf644a832e06b862f57393a6e9011
Ahmad <ahmadxpanahi@gmail.com>
2023-01-23 18:41:42 +0330

    Update number_extension.dart

--- lib/utils/extensions/number_extension.dart
+++ lib/utils/extensions/number_extension.dart
@@ -1,4 +1,66 @@
 extension IntExtesion on int {
+  String getMonthSumName() {
+    switch (this) {
+      case 1:
+        return "JAN";
+      case 2:
+        return "FEB";
+      case 3:
+        return "MAR";
+      case 4:
+        return "APR";
+      case 5:
+        return "MAY";
+      case 6:
+        return "JUN";
+      case 7:
+        return "JUL";
+      case 8:
+        return "AUG";
+      case 9:
+        return "SEP";
+      case 10:
+        return "OCT";
+      case 11:
+        return "NOV";
+      case 12:
+        return "DEC";
+      default:
+        return "JAN";
+    }
+  }
+
+  String getMonthName() {
+    switch (this) {
+      case 1:
+        return "January";
+      case 2:
+        return "February";
+      case 3:
+        return "March";
+      case 4:
+        return "April";
+      case 5:
+        return "May";
+      case 6:
+        return "June";
+      case 7:
+        return "July";
+      case 8:
+        return "August";
+      case 9:
+        return "September";
+      case 10:
+        return "October";
+      case 11:
+        return "November";
+      case 12:
+        return "December";
+      default:
+        return "January";
+    }
+  }
+
   String toKMB() {
     if (this > 999 && this < 99999)
       return "${(this / 1000).toStringAsFixed(1)} K";
